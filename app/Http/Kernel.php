fc22af93ea593eb6b83f8a8d3d797ebec6bae49c
ali aharian <aliaharian@MacBook-Pro.local>
2023-02-18 12:33:43 +0330

    bugfix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,8 @@ class Kernel extends HttpKernel
             \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
             \App\Http\Middleware\TrimStrings::class,
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+            \Fruitcake\Cors\HandleCors::class,
+
     ];
 
     /**
@@ -46,6 +48,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
                 \Illuminate\Routing\Middleware\SubstituteBindings::class,
+                \Fruitcake\Cors\HandleCors::class,
+
         ],
     ];
 
